565c8396419bab63c87f396209799ce138ed8d5c
Michael Drogalis <mjd3089@rit.edu>
2017-09-12 15:42:40 -0700

    Downgrade Clojure.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/onyx-platform/onyx-cep"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha20"]
-                 [org.onyxplatform/onyx "0.11.0-alpha2"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [org.onyxplatform/onyx "0.11.0-SNAPSHOT"]
                  [org.onyxplatform/metamorphic "0.1.0-SNAPSHOT"]]
   :resource-paths ["test-resources/"])
