0a5dfa87f023cff1fb554a44722d0224baf076d1
Lucas Bradstreet <lucasbradstreet@gmail.com>
2017-09-13 00:42:33 -0700

    Use memory state store for tests.

--- project.clj
+++ project.clj
@@ -3,7 +3,14 @@
   :url "https://github.com/onyx-platform/onyx-cep"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :jvm-opts ^:replace ["-server"
+                       "-Xmx2400M"
+                       "-XX:+UseG1GC" 
+                       "-XX:-OmitStackTraceInFastThrow" 
+                       "-XX:+UnlockCommercialFeatures"
+                       "-XX:+FlightRecorder"
+                       "-XX:StartFlightRecording=duration=1080s,filename=recording.jfr"]
   :dependencies [[org.clojure/clojure "1.9.0-alpha20"]
-                 [org.onyxplatform/onyx "0.11.0-alpha3"]
+                 [org.onyxplatform/onyx "0.11.0-20170913.073312-9"]
                  [org.onyxplatform/metamorphic "0.1.0-SNAPSHOT"]]
   :profiles {:dev {:resource-paths ["test-resources/"]}})
